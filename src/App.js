bb4ae28f5f4b52925ad59aa731acdc3a7416cb89
Ricardo Shinoda <ricardoshinoda@gmail.com>
2022-07-13 22:02:07 -0300

    iniciando novo projeto

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js - inicinado o projeto pessoal</code> and save to reload.
+          Edit <code>src/App.js - iniciando o projeto pessoal</code> and save to reload.
         </p>
         <a
           className="App-link"
