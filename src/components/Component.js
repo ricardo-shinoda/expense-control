cce276de142ffea2afbdcde3a571434b67a79af1
Ricardo Shinoda <ricardoshinoda@gmail.com>
2022-07-18 14:09:56 -0300

    adding components to header filter

--- src/components/Component.js
+++ src/components/Component.js
@@ -4,13 +4,66 @@ import style from '../style/Component.module.css';
 
 function Api() {
     const { data } = useContext(context);
+    const { currency, setCurrency } = useContext(context);
+    const currencies = data;
+
+    // ['UDS', 'USDT', 'CAD', 'GBP', 'ARS', 'BTC', 'LTC', 'EUR', 'JPY', 'CHF', 'AUD', 'CNY', 'ILS', 'EHT', 'XRP', 'DOGE']
     console.log(data);
+
+    const handleChange = ({ target, name }) => {
+        setCurrency({ [name]: target.value })
+    }
+
     return (
-        <div className={ style.Component }>
-            <h1>Esse é o Componente</h1>
-            <p>{data}</p>
+        <div className={style.Component}>
+            <section>
+                <label htmlFor="value">Valor
+                    <input
+                        type="number"
+                        name="value"
+                        id="value"
+                    />
+                </label>
+            </section>
+            <section>
+                <label htmlFor="currency">Moeda
+                    <select
+                        id="moeda"
+                        name="currency"
+                        value={currency}
+                        onChange={handleChange}
+                    >
+                        {currencies.map((cur) => <option key={cur}>{cur}</option>)}
+                    </select>
+                </label>
+            </section>
+            <section>
+                <label>Método de pagamento
+                    <select>
+
+                    </select>
+                </label>
+            </section>
+            <section>
+                <label>Tag
+                    <select>
+
+                    </select>
+                </label>
+            </section>
+            <section>
+                <label>Descrição
+                    <input
+                    type="text">
+                    </input>
+                </label>
+            </section>
+            <section>
+                <button>Adicionar despesa</button>
+            </section>
         </div>
-)};
+    )
+};
 
 
 export default Api;
--- src/service/Api.js
+++ src/service/Api.js
@@ -1,7 +1,9 @@
 const data = async () => {
     const URL = await fetch('https://economia.awesomeapi.com.br/json/all');
     const URLJson = await URL.json();
-    return URLJson;
+    // return URLJson;
+    const URLJsonArr = Object.keys(URLJson).filter((val) => val !== 'USDT');
+    return URLJsonArr;
 }
 
 export default data;
