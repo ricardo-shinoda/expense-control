6e4b1da42c59f71d1c6a7b4de0fe407e93ddce9f
Ricardo Shinoda <ricardoshinoda@gmail.com>
2022-07-27 22:06:19 -0300

    feat: Adiciona o cabeçalho da tabela - req 7

--- src/components/Component.js
+++ src/components/Component.js
@@ -124,6 +124,21 @@ function Api() {
                     </button>
                 </section>
             </form>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Descrição</th>
+                        <th>Tag</th>
+                        <th>Método de pagamento</th>
+                        <th>Valor</th>
+                        <th>Moeda</th>
+                        <th>Câmbio utilizado</th>
+                        <th>Valor convertido</th>
+                        <th>Moeda de conversão</th>
+                        <th>Editar/Excluir</th>
+                    </tr>
+                </thead>
+            </table>
         </div>
     )
 };
